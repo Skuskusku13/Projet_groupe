ee67e6c727f51ab52cd99ec8533cb8c534b79de3
Skuskusku13 <danlevy149@gmail.com>
2022-01-27 21:42:37 +0100

    Changement de titre

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site CFA INSTA</title>
+    <title>Projet groupe Info CFA</title>
     <!-- bootstrap css -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <!-- css -->
@@ -14,7 +14,7 @@
 
 <body>
     <center>
-        <h1>Bienvenue sur le site du CFA INSTA</h1>
+        <h1>Bienvenue sur notre site sur le CFA</h1>
         </br>
         <a href="index.php?page=0">
             <img src="images/home.png" height="40" width="40">
