37cd9831d77abd801f0e2c8f5e1fe3456564f00c
Skuskusku13 <danlevy149@gmail.com>
2022-01-27 21:05:17 +0100

    mise en forme

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,26 +11,30 @@
     <!-- css -->
     <link rel="stylesheet" href="css/style1.css">
 </head>
+
 <body>
-<center>
-    <h1>Bienvenue sur le site du CFA INSTA</h1>
-    </br>
+    <center>
+        <h1>Bienvenue sur le site du CFA INSTA</h1>
+        </br>
         <a href="index.php?page=0">
-        <img src="images/home.png" height="40" width="40">
+            <img src="images/home.png" height="40" width="40">
         </a>
         <a href="index.php?page=1">
-        <img src="images/professeur.png" height="40" width="40">
+            <img src="images/professeur.png" height="40" width="40">
         </a>
         <a href="index.php?page=2">
-        <img src="images/eleve.png" height="40" width="40">
+            <img src="images/eleve.png" height="40" width="40">
         </a>
         <a href="index.php?page=3">
-        <img src="images/classe.png" height="40" width="40">
+            <img src="images/classe.png" height="40" width="40">
         </a>
         <a href="index.php?page=4">
-        <img src="images/matiere.png" height="40" width="40">
-        </a>  
+            <img src="images/matiere.png" height="40" width="40">
+        </a>
         <a href="index.php?page=5">
-        <img  style="object-fit: cover; transform: rotate(180deg);" src="images/deconnexion.png" height="40" width="40">
-        </a>                         
-</center>            
+            <img style="object-fit: cover; transform: rotate(180deg);" src="images/deconnexion.png" height="40" width="40">
+        </a>
+    </center>
+</body>
+
+</html>
