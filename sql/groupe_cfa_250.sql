0817bbc7906a437ccbc8f44941d63d0a5bc9a6bf
Skuskusku13 <danlevy149@gmail.com>
2022-01-28 09:29:56 +0100

    Modification du sql

--- sql/groupe_cfa_250.sql
+++ sql/groupe_cfa_250.sql
@@ -2,6 +2,15 @@ drop database if exists groupe_cfa_250;
 create database groupe_cfa_250;
 use groupe_cfa_250;
 
+create table classe(
+    idclasse int(3) not null auto_increment,
+    nomClasse varchar(50) not null,
+    salle varchar(50) not null,
+    diplome varchar(50),
+    nbEtudiants int(3) not null,
+    primary key (idclasse)
+);
+
 create table etudiant(
     idetudiant int(3) not null auto_increment,
     nom varchar(50) not null,
@@ -10,45 +19,30 @@ create table etudiant(
     email varchar(50) not null,
     mdp varchar(50) not null,
     tel varchar(50),
-    primary key (idetudiant)
-);
-
-create table classe(
-    idclasse int(3) not null auto_increment,
-    salle varchar(50) not null,
-    professeur varchar(50) not null,
-    email varchar(50) not null,
-    tel varchar(50),
-    promo varchar(50) not null,
-    idetudiant int(3) not null,
-    primary key (idclasse),
-    foreign key(idetudiant) references etudiant(idetudiant)
+    idclasse int(3) not null,
+    primary key (idetudiant),
+    foreign key(idclasse) references classe(idclasse)
 );
 
 create table professeur(
     idprofesseur int(3) not null auto_increment,
     nom varchar(50) not null,
     prenom varchar(50) not null,
-    adresse varchar(50),
-    salle varchar(50) not null,
     email varchar(50) not null,
     mdp varchar(50) not null,
     tel varchar(50),
-    idclasse int(3) not null,
-    idetudiant int(3) not null,
-    primary key (idprofesseur),
-    foreign key(idclasse) references classe(idclasse),
-    foreign key(idetudiant) references etudiant(idetudiant)
+    primary key (idprofesseur)
 );
 
 create table matiere(
     idmatiere int(3) not null auto_increment,
     nomMatiere varchar(50) not null,
-    salle varchar(50) not null,
-    idetudiant int(3) not null,
+    coef int(2) not null,
+    nbHeures int(3) not null,
+    idclasse int(3) not null,
     idprofesseur int(3) not null,
     primary key (idmatiere),
-    foreign key(idetudiant) references etudiant(idetudiant),
+    foreign key(idclasse) references classe(idclasse),
     foreign key(idprofesseur) references professeur(idprofesseur)
 );
 
